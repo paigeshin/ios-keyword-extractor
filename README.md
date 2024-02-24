88274de86eb8a35fdca32ecd71bfc4c49ca08907
신승현 <sinseunghyeon@sinseunghyeons-MacBook-Pro.local>
2024-02-25 05:13:29 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
 # ios-keyword-extractor
+
+### How to use it
+
+- Automatically Sorts All Keywords
+- Plurals Removed
+- Duplicates Removed
+- Put all your exisiting keywords to avoid duplicate keywords
+
+```jsx
+const { extractKeywords } = require("extractKeywords");
+
+const inputTitles = [
+  {
+    title: "Bulking with Ideal Protein, Super Power",
+    subtitle: "Gain weight, muscle, meals",
+  },
+  {
+    title: "Arise Bodybuilding meal plan",
+    subtitle: "Body building app, bulq, plate",
+  },
+];
+
+// My Previous Keywords.. You can give Empty Array
+const existingKeywords = [
+  "foodnoms",
+  "bulk",
+  "myprotein",
+  "tracking",
+  "well",
+  "menu",
+  "planner",
+  "my",
+  "fitness",
+  "pal",
+  "myfitnesspal",
+  "calories",
+  "diary",
+  "macros",
+  "clean",
+  "simple",
+  "eats",
+];
+
+const inputKeywords = [
+  "Bulking with Ideal Protein, Super Power",
+  "planning power hahaha kk",
+  "workout",
+  "healthy",
+  "fitnesspal",
+  "fit",
+  "fit",
+  "fit",
+  "fit",
+  "fits",
+  "fitness",
+  "fitnesses",
+  "cycling",
+  "para",
+  "fire",
+  "prep",
+  "foodnoms",
+  "bulk",
+  "myprotein",
+  "tracking",
+  "well",
+  "menu",
+  "planner",
+  "my",
+  "fitness",
+  "pal",
+  "myfitnesspal",
+  "calories",
+  "diary",
+  "macros",
+  "clean",
+  "simple",
+  "eats",
+  "intake",
+  "strength",
+];
+
+const result = extractKeywords(inputTitles, existingKeywords, inputKeywords);
+console.log(result);
+let totalWordCount = result.reduce((acc, word) => acc + word.length, 0);
+console.log(totalWordCount);
+```
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+// Importing utility functions from an external module.
 const {
   duplicatesRemoved,
   redundantPluralsRemoved,
   shuffled,
 } = require("./utils/array-extension");
 
+// Define a function to extract keywords from given titles and subtitles, and process them.
 function extractKeywords(titlesArray, existingKeywords, newKeywords) {
+  // Initialize an array to hold the result keywords.
   let resultKeywords = [];
+  // Initialize a counter for the number of characters in the result keywords.
   let charCount = 0;
 
-  // extract all words from titles and subtitles and add it to existing keywords
+  // Iterate over each title and subtitle in the given array.
   titlesArray.forEach((item) => {
+    // Add words from title and subtitle to the existing keywords, after converting them to lowercase,
+    // removing non-alphanumeric characters, and splitting them into individual words.
     existingKeywords = existingKeywords.concat(
       ...item.title
         .toLowerCase()
@@ -22,38 +28,51 @@ function extractKeywords(titlesArray, existingKeywords, newKeywords) {
     );
   });
 
+  // Shuffle the newKeywords array for randomness.
   let keywords = shuffled(newKeywords);
+  // Remove redundant plurals from the keywords array.
   keywords = redundantPluralsRemoved(keywords);
+  // Remove duplicate keywords.
   keywords = duplicatesRemoved(keywords);
 
+  // Iterate over each keyword in the processed list.
   keywords.forEach((keyword) => {
+    // Split the keyword into individual words after converting it to lowercase and removing non-alphanumeric characters.
     const currentKeywords = keyword
       .toLowerCase()
       .replace(/[^a-zA-Z0-9\s]/g, "")
       .split(" ");
 
+    // Calculate the total character count of the current keyword.
     let totalWordCount = currentKeywords.reduce(
       (acc, word) => acc + word.length,
       0
     );
 
-    // Check if any word in currentKeywords is in extractedTitles or resultKeywords
+    // Check if any word in the current keywords exists either in the existing keywords or result keywords.
     const anyWordExists = currentKeywords.some(
       (word) => existingKeywords.includes(word) || resultKeywords.includes(word)
     );
 
+    // If any word exists, skip to the next keyword.
     if (anyWordExists) {
       return;
     }
 
-    // If totalWordCount + charCount exceeds 100 or any word exists, skip the currentKeywords
+    // If adding the current keyword would exceed 100 characters in total, skip to the next keyword.
     if (charCount + totalWordCount > 100) {
       return;
     }
 
+    // Add the current keywords to the result and update the character count.
     resultKeywords.push(...currentKeywords);
     charCount += totalWordCount;
   });
 
+  // Return the processed list of keywords.
   return resultKeywords;
 }
+
+module.exports = {
+  extractKeywords,
+};
